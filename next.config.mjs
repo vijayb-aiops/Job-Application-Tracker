a74997d763d456a3aedd03304c93e1d448fe81fc
Vijaya Rajeev Bollu <bvijaycloud@gmail.com>
2026-01-14 17:53:22 -0500

    deploy to github pages

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,9 @@
 
 const nextConfig = {
 	turbopack: {},
+	output: "export",
 	images: {
+		unoptimized: true,
 		remotePatterns: [
 			{
 				protocol: "https",
@@ -17,6 +19,8 @@ const nextConfig = {
 		ignoreDuringBuilds: true,
 	},
 	allowedDevOrigins: ["*.theopenbuilder.com"],
+	basePath: "/Job-Application-Tracker",
+	assetPrefix: "/Job-Application-Tracker/",
 };
 
 export default nextConfig;
